6df593f5db6796b5b940f9494e217e0d4c1f8a24
Xinus Wang <xinus.wang@gmail.com>
2014-11-01 18:58:34 +0800

    Update.

--- YouJiaoBao/YouJiaoBao/Controller/CSAssessmentEditorViewController.m
+++ YouJiaoBao/YouJiaoBao/Controller/CSAssessmentEditorViewController.m
@@ -8,9 +8,22 @@
 
 #import "CSAssessmentEditorViewController.h"
 #import "TPKeyboardAvoidingScrollView.h"
+#import "EDStarRating.h"
 
-@interface CSAssessmentEditorViewController ()
+@interface CSAssessmentEditorViewController () <EDStarRatingProtocol>
 @property (weak, nonatomic) IBOutlet TPKeyboardAvoidingScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating0;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating1;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating2;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating3;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating4;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating5;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating6;
+@property (weak, nonatomic) IBOutlet EDStarRating *starRating7;
+@property (weak, nonatomic) IBOutlet UIImageView *imgText1Bg;
+@property (weak, nonatomic) IBOutlet UITextView *textView1;
+@property (weak, nonatomic) IBOutlet UIImageView *imgText2Bg;
+@property (weak, nonatomic) IBOutlet UITextView *textView2;
 
 @end
 
@@ -33,6 +46,38 @@
     [self customizeBackBarItem];
     
     self.scrollView.contentSize = CGSizeMake(self.scrollView.bounds.size.width, 600);
+    self.imgText1Bg.image = [[UIImage imageNamed:@"bg-2.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    self.imgText2Bg.image = [[UIImage imageNamed:@"bg-2.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
+    
+    self.textView1.frame = self.imgText1Bg.frame;
+    self.textView2.frame = self.imgText2Bg.frame;
+    
+    NSArray* starRatings = @[self.starRating0,
+                             self.starRating1,
+                             self.starRating2,
+                             self.starRating3,
+                             self.starRating4,
+                             self.starRating5,
+                             self.starRating6,
+                             self.starRating7];
+    
+    for (EDStarRating* starRating in starRatings) {
+        
+        starRating.frame = CGRectMake(0, 64, 64, 28);
+        starRating.backgroundColor  = [UIColor clearColor];
+        starRating.maxRating = 3.0;
+        starRating.delegate = self;
+        starRating.horizontalMargin = 2.0;
+        starRating.editable=YES;
+        starRating.rating = 3;
+        starRating.displayMode=EDStarRatingDisplayFull;
+
+        starRating.starImage = [UIImage imageNamed:@"star_grey.png"];
+        starRating.starHighlightedImage = [UIImage imageNamed:@"star_red.png"];
+        
+        [starRating setNeedsDisplay];
+        starRating.tintColor = [UIColor orangeColor];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,4 +97,9 @@
 }
 */
 
+-(void)starsSelectionChanged:(EDStarRating*)control rating:(float)rating {
+    if (rating < 1) {
+        control.rating = 1;
+    }
+}
 @end
